0a1cd32354db4b57d127f607613e2a8c0eca9c50
Your Name <you@example.com>
2022-04-28 14:36:32 +0300

    header right side and image from redux

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,23 @@
 import "./style.css";
 import { Link } from "react-router-dom";
 import {
-  Friends,
-  Gaming,
-  HomeActive,
-  Logo,
-  Market,
-  Search,
-  Watch,
+    ArrowDown,
+    Friends,
+    Gaming,
+    HomeActive,
+    Logo,
+    Market,
+    Menu,
+    Messenger,
+    Notifications,
+    Search,
+    Watch,
 } from "../../svg";
+import { useSelector } from "react-redux";
 export default function Header() {
+
+    const { user } = useSelector((user) => ({ ...user }));
+
   const color = "#65676b";
   return (
     <header>
@@ -46,7 +54,27 @@ export default function Header() {
           <Gaming color={color} />
         </Link>
       </div>
-      <div className="header_right"></div>
+      
+      <div className="header_right">
+        <Link to="/profile" className="profile_link hover1">
+          <img src={user?.picture} alt="" />
+          <span>{user?.first_name}</span>
+        </Link>
+        <div className="circle_icon hover1">
+          <Menu />
+        </div>
+        <div className="circle_icon hover1">
+          <Messenger />
+        </div>
+        <div className="circle_icon hover1">
+          <Notifications />
+          <div className="right_notification">5</div>
+        </div>
+        <div className="circle_icon hover1">
+          <ArrowDown />
+        </div>
+      </div>
+
     </header>
   );
 }
