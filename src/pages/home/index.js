49b8a941f8d43930f76d76aab6a59bf862e6c7d0
Your Name <you@example.com>
2022-04-28 15:17:32 +0300

    click outside pactice

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,49 @@
 import Header from "../../components/header";
-
+import { useEffect,useRef,useState } from "react";
+import './style.css'
 export default function Home() {
+
+// click outside hide div
+
+const helloRef = useRef(null);
+
+const [show, setShow] = useState(true);
+
+
+useEffect(() => {
+
+  const handleClickOutside = (event) => {
+
+
+    if (helloRef.current && !helloRef.current.contains(event.target)) {
+
+      console.log("helloRef.current",helloRef.current);
+      setShow(false);
+
+    }
+
+  };
+
+  document.addEventListener("mousedown", handleClickOutside);
+
+  // return () => {
+
+  //   document.removeEventListener("mousedown", handleClickOutside);
+
+  // };
+
+
+}, [helloRef]);
+
+
   return (
     <div>
       <Header />
+
+  {show &&  <div className="outside"  ref={helloRef}>
+
+</div>
+}
     </div>
   );
 }
